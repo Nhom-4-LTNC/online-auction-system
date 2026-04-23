78a6b7aec9aef801e1a0b4354cabfac8281361e5
RadiantDolphin <6a8tuandat2019@gmail.com>
2026-04-23 18:40:28 +0700

    Update Entity.java

--- src/main/java/com/auction/model/Entity.java
+++ src/main/java/com/auction/model/Entity.java
@@ -5,12 +5,9 @@ import java.io.Serializable;
 public abstract class Entity implements Serializable {
     private static final long serialVersionUID = -3691692181672595753L;
 
-    protected Integer id;
+    protected Integer id = null;
 
     // CONSTRUCTORS
-    public Entity() {
-        id = null;
-    }
     public Entity(int id) {
         this.id = id;
     }
