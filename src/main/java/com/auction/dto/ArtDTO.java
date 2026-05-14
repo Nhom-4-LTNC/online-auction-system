888dc9e8b2211fb2575e8c19b3b9cf0b62c4efac
PTCuong1052 <153753830+PTCuong1052@users.noreply.github.com>
2026-05-14 11:06:44 +0700

    refactor: ItemFactory

---
+++ src/main/java/com/auction/dto/ArtDTO.java
@@ -0,0 +1,17 @@
+package com.auction.dto;
+
+import com.auction.model.item.ItemType;
+
+public class ArtDTO extends ItemDTO {
+    private final String artist;
+    private final int yearCreated;
+
+    public ArtDTO(String name, String description, double startingPrice, String artist, int yearCreated) {
+        super(ItemType.ART, name, description, startingPrice);
+        this.artist = artist;
+        this.yearCreated = yearCreated;
+    }
+
+    public String getArtist() { return artist; }
+    public int getYearCreated() { return yearCreated; }
+}
